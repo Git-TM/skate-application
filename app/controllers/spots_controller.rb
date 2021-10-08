8c996a58988a876c361ea868bd01aaf79e69439e
Dustin Stender <dustinpstender@gmail.com>
2021-10-08 17:36:00 +0200

    added mapbox

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -1,6 +1,15 @@
 class SpotsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :index ]
+  skip_before_action :authenticate_user!, only: [:index]
 
   def index
+    @spots = Spot.all
+
+    # the `geocoded` scope filters only flats with coordinates (latitude & longitude)
+    @markers = @spots.geocoded.map do |spot|
+      {
+        lat: spot.latitude,
+        lng: spot.longitude
+      }
+    end
   end
 end
