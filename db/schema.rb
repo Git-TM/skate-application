e10c3a297851d645d1e63b6f8bbc0949bcc8dd89
Dustin Stender <dustinpstender@gmail.com>
2021-10-09 17:57:34 +0200

    changed a little css

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-# Active record schema for my branch Cloudinary
-# ActiveRecord::Schema.define(version: 2021_10_08_151031) do
-
-ActiveRecord::Schema.define(version: 2021_10_08_144619) do
+ActiveRecord::Schema.define(version: 2021_10_08_151031) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,9 +56,9 @@ ActiveRecord::Schema.define(version: 2021_10_08_144619) do
     t.integer "score_popularity"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "name"
     t.float "latitude"
     t.float "longitude"
+    t.string "name"
   end
 
   create_table "users", force: :cascade do |t|
